1af7393d64af75d06a984e020f7735c90f960675
Alfred Zhang <alfred@macxdemacbook-pro-7.local>
2016-06-15 09:10:33 +0800

    lesson2

--- Happiness-Demo/FaceView.swift
+++ Happiness-Demo/FaceView.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
+@IBDesignable
 class FaceView: UIView {
 
-   
+   @IBInspectable
     var lineWidth: CGFloat = 3 { didSet { setNeedsDisplay()}}
- 
+@IBInspectable
     var color: UIColor = UIColor.blueColor() { didSet { setNeedsDisplay()}}
-   
+   @IBInspectable
     var scale: CGFloat = 0.9 { didSet { setNeedsDisplay()}}
     
     var faceCenter: CGPoint {
