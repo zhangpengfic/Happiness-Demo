eb74ee5af78fcc0644a2e5f1be250bd40990ea28
Alfred <zhangpeng.fic@163.com>
2016-06-20 11:14:00 +0800

    add branch PsychologistViewController

---
+++ Happiness-Demo/PsychologistViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PsychologistViewController.swift
+//  Happiness-Demo
+//
+//  Created by Alfred on 16/6/20.
+//  Copyright © 2016年 WanXunAlfred. All rights reserved.
+//
+
+import UIKit
+
+class PsychologistViewController: UIViewController {
+
+  
+
+}
